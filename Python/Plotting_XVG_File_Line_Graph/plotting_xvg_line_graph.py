9803422ad4cfa68b3dce61413f7b7efd4b8c9499
Soheilj71 <soheil.jamali.dev@gmail.com>
2025-08-29 13:45:10 -0500

    Putting plotting xvg plotting script

---
+++ Python/Plotting_XVG_File_Line_Graph/plotting_xvg_line_graph.py
@@ -0,0 +1,21 @@
+# Written by Soheil Jamali
+# Email: sjamali@uark.edu, soheil.jamali.dev@gmail.com
+
+#Loading the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load data, skipping comment lines
+data = np.loadtxt('pressure.xvg', comments=['@', '#']) #You can paste any .xvg file here
+
+# Assuming first column is x and second is y
+x = data[:, 0]
+y = data[:, 1]
+
+plt.plot(x, y)
+plt.xlabel('Time (ps)') #You can change it based on your xvg file (e.g., Time, Distance, RMSD, etc.)
+plt.ylabel('Pressure (bar)') #You can change it based on your xvg file (e.g., Energy, RMSD, etc.)
+plt.title('Pressure, NPT Equilibration') #You can change it based on your xvg file (e.g., Energy, RMSD, etc.)
+plt.savefig('pressure.png') #You can change the name of the output figure
+plt.show() #This will display the figure in a pop-up window
+
